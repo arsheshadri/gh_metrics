e8247bb3c88d6412c47511d93700fb448c961f35
arsheshadri <48511582+arsheshadri@users.noreply.github.com>
2024-05-05 14:52:39 +0530

    Update gh_metrics.py

--- scripts/gh_metrics.py
+++ scripts/gh_metrics.py
@@ -103,7 +103,7 @@ for repo_name in repo_names:
            
             # Execute SQL to insert data into Snowflake table
             cursor = conn.cursor()
-            cursor.execute("INSERT INTO GitHub_pull_requests ( Repo_Name,PR_Number,Name_of_incoming_branch,Name_of_target_branch,Datetime_of_merge_request_creation,Name_of_submitter,Name_of_approver,GitHub_team_name,Datetime_of_merge ,Status) VALUES (%s,%s,%s,%s, %s)", (repo_name,pr.number,source_branch,target_branch,pr_datetime,submitter,pr.get_reviews(),null,null,pr.state))
+            cursor.execute("INSERT INTO GitHub_pull_requests ( Repo_Name,PR_Number,Name_of_incoming_branch,Name_of_target_branch,Datetime_of_merge_request_creation,Name_of_submitter,Name_of_approver ,Status) VALUES (%s,%s,%s,%s,%s,%s,%s,%s)", (repo_name,pr.number,source_branch,target_branch,pr_datetime,submitter,pr.get_reviews(),pr.state))
             cursor.close()
             
             # Commit the transaction
