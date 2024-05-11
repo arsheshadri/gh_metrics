1a273999dbbb64a9b2a0b9d6e563807d44226e7e
arsheshadri <48511582+arsheshadri@users.noreply.github.com>
2024-05-11 15:52:50 +0530

    Update github_events.py

--- scripts/github_events.py
+++ scripts/github_events.py
@@ -7,7 +7,8 @@ def main():
     if not org_name or not access_token:
       print("Missing organization name or access token.")
       return None
-     print(f"Retrieving Repositories for Organization- {{ org_name }}")
+    
+    print(f"Retrieving Repositories for Organization- {{ org_name }}")
     repositories = get_organization_repositories(org_name, access_token)
     if repositories:
         print("Repositories in the organization:")
