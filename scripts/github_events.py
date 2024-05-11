a573f7ecdc1542591659b472766a2f0813b0221b
arsheshadri <48511582+arsheshadri@users.noreply.github.com>
2024-05-11 15:28:54 +0530

    Create github_events.py

---
+++ scripts/github_events.py
@@ -0,0 +1,20 @@
+from github_api import get_organization_repositories
+
+def main():
+    org_name = os.environ.get('GITHUB_ORG_NAME')
+    access_token = os.environ.get('GITHUB_ACCESS_TOKEN')
+  
+    if not org_name or not access_token:
+      print("Missing organization name or access token.")
+      return None
+
+    repositories = get_organization_repositories(org_name, access_token)
+    if repositories:
+        print("Repositories in the organization:")
+        for repo in repositories:
+            print(repo["name"])
+    else:
+        print("Failed to fetch repositories.")
+
+if __name__ == "__main__":
+    main()
